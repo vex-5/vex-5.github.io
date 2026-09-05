1d73a0da9d778d3ea1e306aa569ce87fa1f59fdf
juchibu <buitrongtuanlc2012@gmail.com>
2026-09-05 15:44:15 +0700

    497

--- js/full.js
+++ js/full.js
@@ -1,12 +1,53 @@
-function open_fullscreen(){
-    let game = document.getElementById("game-area");
-	if (game.requestFullscreen) {
-	  game.requestFullscreen();
-	} else if (game.mozRequestFullScreen) { /* Firefox */
-	  game.mozRequestFullScreen();
-	} else if (game.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-	  game.webkitRequestFullscreen();
-	} else if (game.msRequestFullscreen) { /* IE/Edge */
-	  game.msRequestFullscreen();
-	}
+function open_fullscreen() {
+    let game = document.getElementById("game-area") || document.querySelector(".game-iframe") || document.querySelector("iframe");
+    if (!game) return;
+
+    let isFullscreen = document.fullscreenElement || 
+                       document.webkitFullscreenElement || 
+                       document.mozFullScreenElement || 
+                       document.msFullscreenElement;
+
+    if (!isFullscreen) {
+        let requestMethod = game.requestFullscreen || 
+                            game.webkitRequestFullscreen || 
+                            game.mozRequestFullScreen || 
+                            game.msRequestFullscreen;
+        if (requestMethod) {
+            let res = requestMethod.call(game);
+            if (res && res.catch) {
+                res.catch(function () {
+                    // Fallback to container if iframe direct fullscreen is restricted
+                    let container = game.closest('.game-iframe-container') || game.parentElement;
+                    if (container) {
+                        let containerReq = container.requestFullscreen || 
+                                           container.webkitRequestFullscreen || 
+                                           container.mozRequestFullScreen || 
+                                           container.msRequestFullscreen;
+                        if (containerReq) {
+                            containerReq.call(container);
+                        }
+                    }
+                });
+            }
+        } else {
+            let container = game.closest('.game-iframe-container') || game.parentElement;
+            if (container) {
+                let containerReq = container.requestFullscreen || 
+                                   container.webkitRequestFullscreen || 
+                                   container.mozRequestFullScreen || 
+                                   container.msRequestFullscreen;
+                if (containerReq) {
+                    containerReq.call(container);
+                }
+            }
+        }
+    } else {
+        let exitMethod = document.exitFullscreen || 
+                         document.webkitExitFullscreen || 
+                         document.mozCancelFullScreen || 
+                         document.msExitFullscreen;
+        if (exitMethod) {
+            exitMethod.call(document);
+        }
+    }
 }
